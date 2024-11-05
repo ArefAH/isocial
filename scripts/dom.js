f07b03f6acdac329afd485c0f55c2c07c9fbebf9
ArefAH <arefabouhamdan@gmail.com>
2024-11-05 22:30:40 +0200

    JavaScript - Added next slide function

---
+++ scripts/dom.js
@@ -0,0 +1,4 @@
+const cardsContainer = document.querySelector(".why-us-cards");
+const cards = document.querySelectorAll(".single-card");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+let currentIndex = 0;
+
+function nextSlide() {
+  cards.forEach((card, index) => {
+    card.classList.toggle("active", index === currentIndex);
+  });
+
+  cards[currentIndex].scrollIntoView({
+    behavior: "smooth",
+    inline: "center",
+  });
+}
+
+nextBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % cards.length;
+  nextSlide();
+});
+
+prevBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+  nextSlide();
+});
+
+nextSlide();
